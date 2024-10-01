645da49949032a4a4bb9a1819351a3330491e195
NULLVOID76 <nullvoid76@gmail.com>
2024-10-01 20:10:28 +0530

    version fix

--- messageSender.js
+++ messageSender.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const WHATSAPP_API_URL = `https://graph.facebook.com/${process.env.VERSION}/${process.env.PHONE_NUMBER_ID}/messages?access_token=${process.env.WHATSAPP_ACCESS_TOKEN}`;
+const WHATSAPP_API_URL = `https://graph.facebook.com/v13.0/${process.env.PHONE_NUMBER_ID}/messages?access_token=${process.env.WHATSAPP_ACCESS_TOKEN}`;
 
 // Send a simple text message
 export const sendTextMessage = (to, text) => {
