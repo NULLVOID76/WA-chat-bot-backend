13985b36490c091b198d17e73e08cbd91477cf55
NULLVOID76 <nullvoid76@gmail.com>
2024-10-01 20:07:31 +0530

    axios debug

--- messageSender.js
+++ messageSender.js
@@ -5,19 +5,23 @@ const WHATSAPP_API_URL = `https://graph.facebook.com/${process.env.VERSION}/${pr
 // Send a simple text message
 export const sendTextMessage = (to, text) => {
   
+  console.log(WHATSAPP_API_URL);
   const data = {
     messaging_product: 'whatsapp',
     to: to,
     type: 'text',
     text: { body: text },
   };
-  console.log(WHATSAPP_API_URL);
   console.log(data);
   
-  
-  axios.post(WHATSAPP_API_URL, data)
-    .then(response => console.log('Message sent:', response.data))
-    .catch(error => console.error('Error sending message:', error.response ? error.response.data : error.message));
+  axios({
+        method: "POST",
+        url: WHATSAPP_API_URL,
+        data: data
+      });
+  // axios.post(WHATSAPP_API_URL, data)
+  //   .then(response => console.log('Message sent:', response.data))
+  //   .catch(error => console.error('Error sending message:', error.response ? error.response.data : error.message));
 };
 
 // Send interactive button message
@@ -38,7 +42,12 @@ export const sendButtonMessage = (to, text, buttons) => {
     },
   };
 
-  axios.post(WHATSAPP_API_URL, data)
-    .then(response => console.log('Button message sent:', response.data))
-    .catch(error => console.error('Error sending button message:', error.response ? error.response.data : error.message));
+  axios({
+    method: "POST",
+    url: WHATSAPP_API_URL,
+    data: data
+  });
+  // axios.post(WHATSAPP_API_URL, data)
+  //   .then(response => console.log('Button message sent:', response.data))
+  //   .catch(error => console.error('Error sending button message:', error.response ? error.response.data : error.message));
 };
